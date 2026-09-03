1ff78d89359740aacae35726a0c1be10ce4585f4
Sumit Wade <sumitwade1111@gmail.com>
2026-09-03 15:34:38 +0530

    Add Internet Gateway module

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ module "vpc" {
   vpc_name = var.vpc_name
   vpc_cidr = var.vpc_cidr
 }
+module "igw" {
+  source = "./modules/internet_gateway"
+
+  vpc_id = module.vpc.vpc_id
+  name   = "main-igw"
+}
