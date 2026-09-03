cc188bfc547b0df06b66c7d30cc704ad84c8ce30
Sumit Wade <sumitwade1111@gmail.com>
2026-09-03 22:01:02 +0530

    Add subnet module

--- main.tf
+++ main.tf
@@ -9,3 +9,23 @@ module "igw" {
   vpc_id = module.vpc.vpc_id
   name   = "main-igw"
 }
+
+module "public_subnet_1" {
+  source = "./modules/subnet"
+
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  public_ip         = true
+  name              = "public-subnet-1"
+}
+
+module "public_subnet_2" {
+  source = "./modules/subnet"
+
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  public_ip         = true
+  name              = "public-subnet-2"
+}
--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,10 @@ output "vpc_id" {
 output "igw_id" {
   value = module.igw.igw_id
 }
+output "public_subnet_1_id" {
+  value = module.public_subnet_1.subnet_id
+}
+
+output "public_subnet_2_id" {
+  value = module.public_subnet_2.subnet_id
+}
